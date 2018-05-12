61b6b90906f73058fd452ae1cc7b32baf1c4d8ef
nyamuk <asamsulfat13@gmail.com>
2018-05-12 23:57:44 +0700

    add recycler view for display kelas

--- app/src/main/java/poros/filkom/ub/jadwalujianfilkom/HomeActivity.java
+++ app/src/main/java/poros/filkom/ub/jadwalujianfilkom/HomeActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import poros.filkom.ub.jadwalujianfilkom.adapter.JadwalAdapter;
 import poros.filkom.ub.jadwalujianfilkom.model.Cell;
 import poros.filkom.ub.jadwalujianfilkom.model.DetailJadwal;
 import poros.filkom.ub.jadwalujianfilkom.model.JadwalResponse;
@@ -25,10 +28,13 @@ public class HomeActivity extends AppCompatActivity {
 
     public static final String TAG = "hmm";
     private ArrayList<DetailJadwal> jadwals;
-    private static ArrayList<DetailJadwal> jadwalKu =  new ArrayList<>();
     private String[] daftarHari = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Senin", "Rabu", "Kamis"};
     static String ruangan = "";
     static String prodi = "";
+
+    private RecyclerView recyclerView;
+    private JadwalAdapter jadwalAdapter;
+    private static ArrayList<DetailJadwal> jadwalKu =  new ArrayList<>();
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
@@ -37,6 +43,10 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        jadwalAdapter = new JadwalAdapter(getApplicationContext());
 
         Intent intent = getIntent();
         Bundle args = intent.getBundleExtra("BUNDLE");
@@ -63,6 +73,9 @@ public class HomeActivity extends AppCompatActivity {
             Log.d(TAG, "getJadwalKu: "+jadwalKu.get(i).getMatkul());
             Log.d(TAG, "getJadwalKu: "+jadwalKu.get(i).getRuang());
         }
+        jadwalAdapter = new JadwalAdapter(this);
+        jadwalAdapter.addItem(jadwalKu);
+        recyclerView.setAdapter(jadwalAdapter);
     }
 
     private void getJadwal() {
@@ -78,83 +91,99 @@ public class HomeActivity extends AppCompatActivity {
                 for (int h = 0; h < jadwalResponse.getPages().size(); h++) {
                     //Log.d(TAG, "Hari "+h+": ========================");
                     for (int i = 1; i < jadwalResponse.getPages().get(h).getTables().get(0).getCells().size(); i++) {
-                    ArrayList<Cell> cell = (ArrayList<Cell>) response.body().getPages().get(h).getTables().get(0).getCells();
-                    if (cell.get(i).getI() <= 2) {
+                        ArrayList<Cell> cell = (ArrayList<Cell>) response.body().getPages().get(h).getTables().get(0).getCells();
+                        if (cell.get(i).getI() <= 2) {
 
-                    } else {
-                        if (cell.get(i).getJ().toString().equals("1")) {
-                            ruangan = cell.get(i).getContent();
+                        } else {
+                            if (cell.get(i).getJ().toString().equals("1")) {
+                                ruangan = cell.get(i).getContent();
 //                            //Log.d(TAG, "onResponse: ==============================");
 //                            //Log.d(TAG, "JA Ruangan: "+cell.get(i).getContent());
-                        }
-                        switch (prodi) {
-                            case "Teknik Informatika":
-                                prodi = "TIF";
-                                break;
-                        }
-                        switch (cell.get(i).getJ().toString()) {
-                            default:
-                                //ruangan = cell.get(i).getContent();
+                            }
+                            switch (prodi) {
+                                case "Magister Ilmu Komputer":
+                                    prodi = "MIK";
+                                    break;
+                                case "Teknik Informatika":
+                                    prodi = "TIF";
+                                    break;
+                                case "Teknik Komputer":
+                                    prodi = "TEKOM";
+                                    break;
+                                case "Pendidikan Teknologi Informasi":
+                                    prodi = "PTI";
+                                    break;
+                                case "Sistem Informasi":
+                                    prodi = "SI";
+                                    break;
+                                case "Teknologi Informasi":
+                                    prodi = "TI";
+                                    break;
+                            }
+
+                            switch (cell.get(i).getJ().toString()) {
+                                default:
+                                    //ruangan = cell.get(i).getContent();
 //                            case "2":
 //                                Log.d(TAG, "JA Prodi: "+cell.get(i).getContent());
 //                                break;
-                            case "3":
-                                for (int j = 0; j < jadwals.size(); j++) {
-                                    if (
-                                            prodi.equals(cell.get(i-1).getContent().trim()) &&
-                                            jadwals.get(j).getMatkul().trim().equals(cell.get(i).getContent().trim()) &&
-                                            jadwals.get(j).getKelas().trim().equals(cell.get(i+1).getContent().trim())) {
-                                        Log.d(TAG, "JA Ruangan: "+ruangan);
-                                        Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
-                                        Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
-                                        DetailJadwal detailJadwal = new DetailJadwal();
-                                        detailJadwal.setHari(daftarHari[h]);
-                                        detailJadwal.setJam("pagi");
-                                        detailJadwal.setKelas(cell.get(i+1).getContent());
-                                        detailJadwal.setMatkul(cell.get(i).getContent());
-                                        detailJadwal.setRuang(ruangan);
-                                        jadwalKu.add(ke, detailJadwal);
-                                        ke++;
+                                case "3":
+                                    for (int j = 0; j < jadwals.size(); j++) {
+                                        if (
+                                                prodi.equals(cell.get(i-1).getContent().trim()) &&
+                                                        jadwals.get(j).getMatkul().trim().equals(cell.get(i).getContent().trim()) &&
+                                                        jadwals.get(j).getKelas().trim().equals(cell.get(i+1).getContent().trim())) {
+                                            Log.d(TAG, "JA Ruangan: "+ruangan);
+                                            Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
+                                            Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
+                                            DetailJadwal detailJadwal = new DetailJadwal();
+                                            detailJadwal.setHari(daftarHari[h]);
+                                            detailJadwal.setJam("pagi");
+                                            detailJadwal.setKelas(cell.get(i+1).getContent());
+                                            detailJadwal.setMatkul(cell.get(i).getContent());
+                                            detailJadwal.setRuang(ruangan);
+                                            jadwalKu.add(ke, detailJadwal);
+                                            ke++;
+                                        }
                                     }
-                                }
-                                break;
+                                    break;
 //                            case "4":
 //                                Log.d(TAG, "JA kelas: "+cell.get(i).getContent());
 //                                break;
 //                            case "5":
 //                                Log.d(TAG, "JA Prodi: "+cell.get(i).getContent());
 //                                break;
-                            case "6":
-                                // 6 = matkul, 7 = kelas
-                                //int cases = 7;
+                                case "6":
+                                    // 6 = matkul, 7 = kelas
+                                    //int cases = 7;
 
 //                                    Log.d(TAG, "onResponse: ------------------------------");
 //                                                                            Log.d(TAG, "JA siam : "+jadwals.get(j).getMatkul());
 //                                        Log.d(TAG, "JA siamk: "+jadwals.get(j).getKelas());
 //                                    Log.d(TAG, "onResponse: 99999999999999999");
-                                //        Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
-                                //Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
+                                    //        Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
+                                    //Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
 //                                    Log.d(TAG, "onResponse: ------------------------------");
 
-                                for (int j = 0; j < jadwals.size(); j++) {
-                                    if (
-                                            prodi.equals(cell.get(i-1).getContent().trim()) &&
-                                            jadwals.get(j).getMatkul().trim().equals(cell.get(i).getContent().trim()) &&
-                                            jadwals.get(j).getKelas().trim().equals(cell.get(i+1).getContent().trim())) {
-                                        Log.d(TAG, "JA Ruangan: "+ruangan);
-                                        Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
-                                        Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
-                                        DetailJadwal detailJadwal = new DetailJadwal();
-                                        detailJadwal.setHari(daftarHari[h]);
-                                        detailJadwal.setJam("siang");
-                                        detailJadwal.setKelas(cell.get(i+1).getContent());
-                                        detailJadwal.setMatkul(cell.get(i).getContent());
-                                        detailJadwal.setRuang(ruangan);
-                                        jadwalKu.add(ke, detailJadwal);
-                                        ke++;
+                                    for (int j = 0; j < jadwals.size(); j++) {
+                                        if (
+                                                prodi.equals(cell.get(i-1).getContent().trim()) &&
+                                                        jadwals.get(j).getMatkul().trim().equals(cell.get(i).getContent().trim()) &&
+                                                        jadwals.get(j).getKelas().trim().equals(cell.get(i+1).getContent().trim())) {
+                                            Log.d(TAG, "JA Ruangan: "+ruangan);
+                                            Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
+                                            Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
+                                            DetailJadwal detailJadwal = new DetailJadwal();
+                                            detailJadwal.setHari(daftarHari[h]);
+                                            detailJadwal.setJam("siang");
+                                            detailJadwal.setKelas(cell.get(i+1).getContent());
+                                            detailJadwal.setMatkul(cell.get(i).getContent());
+                                            detailJadwal.setRuang(ruangan);
+                                            jadwalKu.add(ke, detailJadwal);
+                                            ke++;
+                                        }
                                     }
-                                }
-                                break;
+                                    break;
 //                            case "7":
 //                                Log.d(TAG, "JA kelas: "+cell.get(i).getContent());
 //                                break;
@@ -163,40 +192,40 @@ public class HomeActivity extends AppCompatActivity {
 
 
 //                                break;
-                            case "9":
-                                //Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
-                                for (int j = 0; j < jadwals.size(); j++) {
-                                    if (
-                                            prodi.equals(cell.get(i-1).getContent().trim()) &&
-                                            jadwals.get(j).getMatkul().trim().equals(cell.get(i).getContent().trim()) &&
-                                            jadwals.get(j).getKelas().trim().equals(cell.get(i+1).getContent().trim())) {
-                                        Log.d(TAG, "JA Ruangan: "+ruangan);
-                                        Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
-                                        Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
-                                        DetailJadwal detailJadwal = new DetailJadwal();
-                                        detailJadwal.setHari(daftarHari[h]);
-                                        detailJadwal.setJam("sore");
-                                        detailJadwal.setKelas(cell.get(i+1).getContent());
-                                        detailJadwal.setMatkul(cell.get(i).getContent());
-                                        detailJadwal.setRuang(ruangan);
-                                        jadwalKu.add(ke, detailJadwal);
-                                        ke++;
+                                case "9":
+                                    //Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
+                                    for (int j = 0; j < jadwals.size(); j++) {
+                                        if (
+                                                prodi.equals(cell.get(i-1).getContent().trim()) &&
+                                                        jadwals.get(j).getMatkul().trim().equals(cell.get(i).getContent().trim()) &&
+                                                        jadwals.get(j).getKelas().trim().equals(cell.get(i+1).getContent().trim())) {
+                                            Log.d(TAG, "JA Ruangan: "+ruangan);
+                                            Log.d(TAG, "JA Matkul: "+cell.get(i).getContent());
+                                            Log.d(TAG, "JA kelas: "+cell.get(i+1).getContent());
+                                            DetailJadwal detailJadwal = new DetailJadwal();
+                                            detailJadwal.setHari(daftarHari[h]);
+                                            detailJadwal.setJam("sore");
+                                            detailJadwal.setKelas(cell.get(i+1).getContent());
+                                            detailJadwal.setMatkul(cell.get(i).getContent());
+                                            detailJadwal.setRuang(ruangan);
+                                            jadwalKu.add(ke, detailJadwal);
+                                            ke++;
+                                        }
                                     }
-                                }
-                                break;
+                                    break;
 //                            case "10":
 //                                Log.d(TAG, "JA Kelas: "+cell.get(i).getContent());
 //                                break;
+                            }
                         }
-                    }
 
 //                    if (cell.get(i).getJ().equals("3") || cell.get(i).getJ().equals("6") || cell.get(i).getJ().equals("9")) {
 //                    }
 //                    if (cell.get(i).getJ().equals("4")) {
 //                        Log.d(TAG, "JA kelas: "+cell.get(i).getContent());
 //                    }
-                    //Log.d("zxcv", "onResponse: " + response.body().getPages().get(0).getTables().get(0).getCells().get(i).getContent());
-                }
+                        //Log.d("zxcv", "onResponse: " + response.body().getPages().get(0).getTables().get(0).getCells().get(i).getContent());
+                    }
 
                 }
 
---
+++ app/src/main/java/poros/filkom/ub/jadwalujianfilkom/adapter/JadwalAdapter.java
@@ -0,0 +1,80 @@
+package poros.filkom.ub.jadwalujianfilkom.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import poros.filkom.ub.jadwalujianfilkom.R;
+import poros.filkom.ub.jadwalujianfilkom.model.DetailJadwal;
+
+public class JadwalAdapter extends RecyclerView.Adapter<JadwalAdapter.ViewHolder> {
+
+    private LayoutInflater layoutInflater;
+    private Context context;
+    private ArrayList<DetailJadwal> jadwalku;
+
+    public JadwalAdapter(Context context) {
+        this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.context = context;
+    }
+
+    public void addItem(ArrayList<DetailJadwal> jadwalku) {
+        this.jadwalku = jadwalku;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        layoutInflater = LayoutInflater.from(parent.getContext());
+        View v = layoutInflater.inflate(R.layout.row_menu, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvHari.setText(jadwalku.get(position).getHari());
+        holder.tvKelas.setText(jadwalku.get(position).getKelas());
+        holder.tvMatkul.setText(jadwalku.get(position).getMatkul());
+        holder.tvJam.setText(jadwalku.get(position).getJam());
+        holder.tvRuang.setText(jadwalku.get(position).getRuang());
+        holder.btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return jadwalku.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvHari, tvKelas, tvMatkul, tvJam, tvRuang;
+        private Button btnDetail;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvHari = (TextView) itemView.findViewById(R.id.tv_hari);
+            tvJam = (TextView) itemView.findViewById(R.id.tv_jam);
+            tvKelas = (TextView) itemView.findViewById(R.id.tv_kelas);
+            tvMatkul = (TextView) itemView.findViewById(R.id.tv_matkul);
+            tvRuang = (TextView) itemView.findViewById(R.id.tv_ruang);
+            btnDetail = (Button) itemView.findViewById(R.id.btn_detail);
+        }
+    }
+}
