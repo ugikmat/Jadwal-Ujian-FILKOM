a3ef5ff008e6852e9db3a3cbdb17e111a70ef854
nyamuk <asamsulfat13@gmail.com>
2018-05-19 06:29:21 +0700

    remove profil tab

--- app/src/main/java/poros/filkom/ub/jadwalujianfilkom/helper/BottomNavigationPosition.kt
+++ app/src/main/java/poros/filkom/ub/jadwalujianfilkom/helper/BottomNavigationPosition.kt
@@ -12,25 +12,25 @@ import poros.filkom.ub.jadwalujianfilkom.fragment.SettingFragment
 
 enum class BottomNavigationPosition(val position: Int, val id: Int) {
     HOME(0, R.id.home),
-    DASHBOARD(1, R.id.dashboard),
-    PROFILE(2, R.id.profile);
+    DASHBOARD(1, R.id.dashboard);
+    //PROFILE(2, R.id.profile);
 }
 
 fun findNavigationPositionById(id: Int): BottomNavigationPosition = when (id) {
     BottomNavigationPosition.HOME.id -> BottomNavigationPosition.HOME
     BottomNavigationPosition.DASHBOARD.id -> BottomNavigationPosition.DASHBOARD
-    BottomNavigationPosition.PROFILE.id -> BottomNavigationPosition.PROFILE
+    //BottomNavigationPosition.PROFILE.id -> BottomNavigationPosition.PROFILE
     else -> BottomNavigationPosition.HOME
 }
 
 fun BottomNavigationPosition.createFragment(): Fragment = when (this) {
     BottomNavigationPosition.HOME -> JadwalFragment.newInstance()
     BottomNavigationPosition.DASHBOARD -> FeedbackFragment.newInstance()
-    BottomNavigationPosition.PROFILE -> SettingFragment.newInstance()
+    //BottomNavigationPosition.PROFILE -> SettingFragment.newInstance()
 }
 
 fun BottomNavigationPosition.getTag(): String = when (this) {
     BottomNavigationPosition.HOME -> JadwalFragment.TAG
     BottomNavigationPosition.DASHBOARD -> FeedbackFragment.TAG
-    BottomNavigationPosition.PROFILE -> SettingFragment.TAG
+    //BottomNavigationPosition.PROFILE -> SettingFragment.TAG
 }
